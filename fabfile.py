a9104e37c09d872bb419d612594945001dd32cc7
Milton Madanda <milton@praekeltfoundation.org>
2012-04-22 13:04:56 +0200

    added push to fab

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,10 @@ env.path = '/var/sites/newsconnector'
 env.supervisord_file = 'supervisord.production.conf'
 env.hosts = ['ubuntu@23.21.46.126']
 
+def push():
+    with cd(env.path):
+        run('git pull')
+
 def deploy():
     with cd(env.path):
         run('git pull')
