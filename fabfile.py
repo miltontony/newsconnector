9499557e833fb8d9c364d32cfcc1ed44f4b559cb
Milton <milton@praekeltfoundation.org>
2012-12-01 16:46:11 +0200

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,16 +4,18 @@ env.path = '/var/sites/newsconnector'
 env.supervisord_file = 'supervisord.conf'
 env.hosts = ['ubuntu@23.21.46.126']
 
+
 def push():
     with cd(env.path):
         run('git pull')
+
 
 def static():
     with cd(env.path):
         run('ve/bin/python %(path)s/newsconnector/manage.py collectstatic --noinput' % env)
 
+
 def deploy():
     with cd(env.path):
         run('git pull')
-        run('supervisorctl -c config/%(supervisord_file)s restart all' % env)
-
+        run('sudo supervisorctl restart all' % env)
