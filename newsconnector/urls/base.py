e8fd072fadc1c3d4d658e2e58adca375ca887026
Milton <milton@praekeltfoundation.org>
2013-02-21 00:31:43 +0200

    fix for robots

--- newsconnector/urls/base.py
+++ newsconnector/urls/base.py
@@ -12,4 +12,6 @@ urlpatterns = patterns('',
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^health/', views.health, name='health'),
     url(r'^cgi-sys/defaultwebpage.cgi$', redirect_to, {'url': '/'}),
+    url(r'^robots\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
 )
