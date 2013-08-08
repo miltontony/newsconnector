285f501fb4a41839adb722795b8e0712a4605026
Milton <madandat@gmail.com>
2013-08-08 11:20:06 +0200

    update loggers

--- newsconnector/settings.py
+++ newsconnector/settings.py
@@ -100,6 +100,11 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %    (message)s'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
