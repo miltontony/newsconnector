e56a556086a7ce4ce3d4aaa3655a8f00b393f34c
Milton <madandat@gmail.com>
2013-10-16 09:00:10 +0200

    only restart celery

--- restart_app.sh
+++ restart_app.sh
@@ -2,4 +2,4 @@
 sudo supervisorctl stop celery
 /var/sites/newsconnector/ve/bin/python /var/sites/newsconnector/newsconnector/manage.py celery purge
 kill -9 `ps aux | grep celery | awk -F' ' '{print $2.}'`
-sudo supervisorctl restart all
+sudo supervisorctl restart celery
