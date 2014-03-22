f91867799edb4476f56dee0315feb8840e37a3c6
Milton <madandat@gmail.com>
2014-03-22 17:13:55 +0200

    added more tasks

--- newsconnector/management/commands/newstask.py
+++ newsconnector/management/commands/newstask.py
@@ -5,10 +5,29 @@ class Command(BaseCommand):
     help = 'Tasks to update articles'
 
     def handle(self, action, *args, **options):
-        from newsconnector.support.tasks import update_feeds, scrape_articles
+        def default(obj):
+            import datetime
+
+            if isinstance(obj, datetime.datetime):
+                return obj.isoformat()
+            return obj
+        from newsconnector.support.tasks import (
+            update_feeds, scrape_articles, build_similar)
+        from pyes.queryset import generate_model
+        import json
+        ArticleModel = generate_model("newsworld", "article")
 
         if action == 'feeds':
             update_feeds()
 
         if action == 'scrape':
             scrape_articles()
+
+        if action == 'dump':
+            print json.dumps(
+                ArticleModel.objects.all().order_by('-date')[:40],
+                default=default
+            )
+
+        if action == 'similar':
+            build_similar('NewsArticle')
