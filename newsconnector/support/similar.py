d904115e18b9920d58b8ac2e7126bbb6549917a8
Milton <madandat@gmail.com>
2014-03-23 14:41:51 +0200

    ensure distinct

--- newsconnector/support/similar.py
+++ newsconnector/support/similar.py
@@ -195,6 +195,8 @@ def build(tag, limit=200):
 
     history = build_similar(articles, tag)
     for a in history:
+        a['similar'] = list(set(a['similar']))
+        a['seen'] = list(set(a['seen']))
         a.save()
     conn.indices.refresh('newsworld')
     logger.info('[similar] indexing complete for: %s' % tag)
