d0e0e596e0c3490999688753565736783042a8f4
Milton Madanda <madandat@gmail.com>
2014-04-13 10:21:55 +0200

    allow articles to be stored in db

--- newsconnector/models.py
+++ newsconnector/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Article(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     link = models.TextField()
+    hash_key = models.CharField(max_length=32, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    title = models.TextField(null=True, blank=True)
+    image_url = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    source = models.TextField(null=True, blank=True)
+    fulltext = models.TextField(null=True, blank=True)
 
     def __unicode__(self):  # pragma: no cover
         return '%s' % self.link
