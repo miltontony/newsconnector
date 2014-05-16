1647bdce18575b1c4bde9bb882e6c89e8374dc7c
Milton Madanda <madandat@gmail.com>
2014-05-16 14:12:03 +0200

    use django query

--- newsconnector/data/store.py
+++ newsconnector/data/store.py
@@ -1,11 +1,6 @@
-from datetime import datetime, date
-import json
-import redis
-#from pyes import ES
-#conn = ES('127.0.0.1:9200')
-#from pyes.queryset import generate_model
-#ArticleModel = generate_model("newsworld", "article")
+from datetime import datetime, timedelta
 from django.db.models.loading import get_model
+from django.db.models import Count
 
 
 def get_articles(tag, limit=20, start=0):
@@ -15,20 +10,9 @@ def get_articles(tag, limit=20, start=0):
 
 def get_headlines(tag, limit=5):
     model = get_model('newsconnector', tag)
-    articles = model.objects.raw('''
-SELECT DISTINCT *,
-"newsconnector_article"."id",
-COUNT("newsconnector_article_similar"."to_article_id") AS "headlines"
-FROM "newsconnector_newsarticle"
-LEFT OUTER JOIN "newsconnector_article"
-    ON ("newsconnector_newsarticle"."article_ptr_id" = "newsconnector_article"."id")
-LEFT OUTER JOIN "newsconnector_article_similar"
-    ON ("newsconnector_article"."id" = "newsconnector_article_similar"."from_article_id")
-WHERE "newsconnector_article"."date" >= '%s'
-GROUP BY "newsconnector_article"."id",
-         "newsconnector_newsarticle"."article_ptr_id",
-         "newsconnector_article_similar"."id"
-ORDER BY "headlines" DESC''' % date.today().isoformat())
+    articles = model.objects.filter(
+        date__gte=datetime.now()-timedelta(days=1)
+    ).annotate(headlines=Count('similar')).order_by('-headlines')
     return articles[:limit]
 
 
