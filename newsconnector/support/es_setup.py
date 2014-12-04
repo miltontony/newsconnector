579be066e16303cb599eed177ac957d3757a58fc
Milton Madanda <madandat@gmail.com>
2014-12-04 08:09:08 +0200

    fix up es setup

--- newsconnector/support/es_setup.py
+++ newsconnector/support/es_setup.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from pyes import ES
 from pyes.queryset import generate_model
-ArticleModel = generate_model("newsworld", "article")
 
 from newsconnector.models import (
     Article, NewsArticle, FinanceArticle, EntertainmentArticle, SportsArticle,
@@ -13,13 +12,26 @@ conn = ES('127.0.0.1:9200')
 
 
 def delete_index():
-    try:
-        conn.indices.delete_index('newsworld')
-    except:
-        pass
+    for model in [NewsArticle, FinanceArticle, SportsArticle,
+                  EntertainmentArticle, INewsArticle, ISportsArticle]:
+        index = model.__name__.lower()
+        try:
+            conn.indices.delete_index(index)
+        except:
+            pass
 
-    conn.indices.create_index("newsworld")
-    conn.indices.default_indices = ["newsworld"]
+    conn.indices.refresh()
+
+
+def create_index():
+    indices = []
+    for model in [NewsArticle, FinanceArticle, SportsArticle,
+                  EntertainmentArticle, INewsArticle, ISportsArticle]:
+        index = model.__name__.lower()
+        conn.indices.create_index(index)
+        indices += [index]
+
+    conn.indices.default_indices = indices
     conn.indices.refresh()
 
 
@@ -59,10 +71,6 @@ def setup():
             'type': u'date'},
         u'date_added': {
             'store': 'yes',
-            'type': u'date'},
-        u'date_added': {
-            'index': 'no',
-            'store': 'no',
             'type': u'date'},
         u'source': {
             'index': 'no',
@@ -146,68 +154,35 @@ def setup():
     }
 
     conn.indices.put_mapping("article", {
-        'settings': {"number_of_shards": 1},
         'properties': mapping,
     })
     conn.indices.refresh()
 
 
 def init():
-    print '0%'
-    for a in NewsArticle.objects.all():
-        conn.index(a.to_json(), 'newsworld', 'article')
-    conn.refresh()
-    print '25%'
-
-    for a in SportsArticle.objects.all():
-        conn.index(a.to_json(), 'newsworld', 'article')
-    conn.refresh()
-    print '50%'
-
-    for a in FinanceArticle.objects.all():
-        conn.index(a.to_json(), 'newsworld', 'article')
-    conn.refresh()
-    print '75%'
-
-    for a in EntertainmentArticle.objects.all():
-        conn.index(a.to_json(), 'newsworld', 'article')
-    conn.refresh()
-    print '100%'
+    for model in [NewsArticle, FinanceArticle, SportsArticle,
+                  EntertainmentArticle, INewsArticle, ISportsArticle]:
+        index = model.__name__.lower()
+        print 'Started %s' % index
+        for a in model.objects.all():
+            conn.index(a.to_json(), index, 'article')
+        conn.refresh()
 
 
 def backup():
     d = datetime.now() - timedelta(days=40)
     print '--- Backup started ---'
+    print 'Deleting all articles in db: %s' % Article.objects.all().count()
     Article.objects.all().delete()
 
-    print 'Backing up NewsArticle'
-    for a in ArticleModel.objects.filter(tag='newsarticle', date__gte=d):
-        NewsArticle.from_es(a)
-    print 'Total: %s' % str(NewsArticle.objects.all().count())
+    for model in [NewsArticle, FinanceArticle, SportsArticle,
+                  EntertainmentArticle, INewsArticle, ISportsArticle]:
+        model_name = model.__name__
+        ArticleModel = generate_model(model_name.lower(), "article")
 
-    print 'Backing up SportsArticle'
-    for a in ArticleModel.objects.filter(tag='sportsarticle', date__gte=d):
-        SportsArticle.from_es(a)
-    print 'Total: %s' % str(SportsArticle.objects.all().count())
-
-    print 'Backing up FinanceArticle'
-    for a in ArticleModel.objects.filter(tag='financearticle', date__gte=d):
-        FinanceArticle.from_es(a)
-    print 'Total: %s' % str(FinanceArticle.objects.all().count())
-
-    print 'Backing up EntertainmentArticle'
-    for a in ArticleModel.objects.filter(tag='entertainmentarticle', date__gte=d):
-        EntertainmentArticle.from_es(a)
-    print 'Total: %s' % str(EntertainmentArticle.objects.all().count())
-
-    print 'Backing up INewsArticle'
-    for a in ArticleModel.objects.filter(tag='inewsarticle', date__gte=d):
-        INewsArticle.from_es(a)
-    print 'Total: %s' % str(INewsArticle.objects.all().count())
-
-    print 'Backing up ISportsArticle'
-    for a in ArticleModel.objects.filter(tag='isportsarticle', date__gte=d):
-        ISportsArticle.from_es(a)
-    print 'Total: %s' % str(ISportsArticle.objects.all().count())
+        print 'Backing up %s' % model.__name__
+        for a in ArticleModel.objects.filter(date__gte=d):
+            model.from_es(a)
+        print 'Total: %s' % str(model.objects.all().count())
 
     print '--- Backup complete ---'
